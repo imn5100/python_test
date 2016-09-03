ccbfe6e89eb63134ddc185e685164c553a507798
imn5100 <xsilence0908xlsw>
2016-09-03 23:14:41 +0800

    complete python  crawler

--- src/dmhyCrawler/Crawler.py
+++ src/dmhyCrawler/Crawler.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 import requests
-import os
 from BeautifulSoup import BeautifulSoup
+from src.dmhyCrawler.DbOperator import DatabaseConnection
+from src.dmhyCrawler.DmhyDataOperator import DmhyDataOperator
+from src.dmhyCrawler.DmhyDataOperator import DmhyData
 
 
 # 这里http请求存在超时问题。10秒 如果url无响应，则抛出timeout异常。（不包括响应的时间）
 def getDMHYHtml(url):
     r = requests.get(url, headers=buildHeader(), timeout=10);
     return r.text
+
 
 def buildHeader():
     headers = {
@@ -24,26 +27,41 @@ def buildHeader():
         'Cookie'] = '__cfduid=d1a9b70e02b3116c622b3747893a44ef81472616279; Hm_lvt_e4918ccc327a268ee93dac21d5a7d53c=1472616271,1472692420,1472779166,1472803144; Hm_lpvt_e4918ccc327a268ee93dac21d5a7d53c=1472803144'
     return headers
 
+
 def analysisHtml(text):
     soup = BeautifulSoup(text)
     body = soup.find('tbody')
+    dmhy_datas = []
     for tr in body.findAll("tr"):
         tds = tr.findAll("td")
+        dmhy = [""]
         time = tds[0].find("span").text
+        dmhy.append(time)
         classi = tds[1].find("font").text
-        title = tds[2].find("a", target="_blank")["href"]
+        dmhy.append(classi)
+        title = tds[2].find("a", target="_blank").text
+        dmhy.append(title)
         magnetLink = tds[3].find("a")["href"]
+        dmhy.append(magnetLink)
         size = tds[4].text
+        dmhy.append(size)
         sendNum = tds[5].find("span").text
+        dmhy.append(sendNum if sendNum.strip() != '' else '0')
         downNum = tds[6].find("span").text
+        dmhy.append(downNum if downNum.strip() != '' else '0')
         comNum = tds[7].text
+        dmhy.append(comNum if comNum.strip() != '' else '0')
         publisher = tds[8].find("a").text
+        dmhy.append(publisher if publisher.strip() != ''else "")
+        dmhy_datas.append(DmhyData(dmhy))
         print(
-        "time:" + time + " classi:" + classi + " title:" + title + " magnetLink:" + magnetLink + " size:" + size + " sendNum:" + sendNum + " downNum:" + downNum + " comNum:" + comNum + " publisher:" + publisher)
+            "time:" + time + " classi:" + classi + " title:" + title + " magnetLink:" + magnetLink + " size:" + size + " sendNum:" + sendNum + " downNum:" + downNum + " comNum:" + comNum + " publisher:" + publisher)
+    return dmhy_datas
+
 
 if __name__ == '__main__':
-    # f = open("E://download//share.dmhy.org.html","r");
-    # size =  os.path.getsize("E://download//share.dmhy.org.html");
-    # text =  f.read(size);
     text = getDMHYHtml("http://share.dmhy.org/topics/list/page/1")
-    analysisHtml(text)
+    dmhydatas = analysisHtml(text)
+    db = DatabaseConnection("127.0.0.1", user="root", passwd='xlsw', db='test')
+    dmop = DmhyDataOperator(db);
+    dmop.add_dmhydata_list(dmhydatas)
--- src/dmhyCrawler/DmhyDataOperator.py
+++ src/dmhyCrawler/DmhyDataOperator.py
@@ -8,44 +8,42 @@ class DmhyDataOperator(object):
         self.db = db
 
     def add_dmhydata(self, dmhydata):
-        sql = "insert into  dmhy(`time`, `classi`, `title`, `magnetLink`, `size`, `seedNum`, `downNum`, `comNum`, `publisher`) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+        sql = "insert into  dmhy(`time`, `classi`, `title`, `magnetLink`, `size`, `seedNum`, `downNum`, `comNum`, `publisher`) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         self.db.put(sql, (
             dmhydata.time, dmhydata.classi, dmhydata.title, dmhydata.magnetLink, dmhydata.size, dmhydata.sendNum,
-            dmhydata.downNum, dmhydata.comNum, dmhydata.publisher))
+            dmhydata.downNum, dmhydata.comNum, dmhydata.publisher,))
 
     def add_dmhydata_list(self, dmhy_data_list):
-        sql = "insert into  dmhy(`time`, `classi`, `title`, `magnetLink`, `size`, `seedNum`, `downNum`, `comNum`, `publisher`) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+        sql = "insert into  dmhy(`time`, `classi`, `title`, `magnetLink`, `size`, `seedNum`, `downNum`, `comNum`, `publisher`) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         params = []
-        i = 0
         for dmhydata in dmhy_data_list:
-            params[i] = (
+            params.append((
                 dmhydata.time, dmhydata.classi, dmhydata.title, dmhydata.magnetLink, dmhydata.size, dmhydata.sendNum,
-                dmhydata.downNum, dmhydata.comNum, dmhydata.publisher)
-            i = i + 1
+                dmhydata.downNum, dmhydata.comNum, dmhydata.publisher))
         self.db.put_many(sql, params)
 
     def getby_title(self, title):
         datas = self.db.get_all("select *  from dmhy where title like %s order by time desc", ('%' + title + '%',))
         dmhydatas = []
-        for  data in datas:
+        for data in datas:
             dmhydataObj = DmhyData(data)
-            dmhydatas.__add__(dmhydataObj)
+            dmhydatas.append(dmhydataObj)
         return dmhydatas;
 
-class DmhyData(object):
-    def __init__(self, id, time, classi, title, magnetLink, size, seedNum=0, downNum=0, comNum=0, publisher=''):
-        self.id = id
-        self.time = time
-        self.classi = classi
-        self.title = title
-        self.magnetLink = magnetLink
-        self.size = size
-        self.sendNum = seedNum
-        self.downNum = downNum
-        self.comNum = comNum
-        self.publisher = publisher
+    def getby_id(self, id):
+        sql = "select * from dmhy where id=%s"
+        data = self.db.get_one(sql, (id,))
+        return DmhyData(data)
 
-    def build(self, data):
+
+class DmhyData(object):
+    def __init__(self):
+        self.sendNum = '0'
+        self.downNum = '0'
+        self.comNum = '0'
+        self.publisher = ''
+
+    def __init__(self, data):
         self.id = data[0]
         self.time = data[1]
         self.classi = data[2]
@@ -61,3 +59,6 @@ class DmhyData(object):
 # test
 if __name__ == '__main__':
     db = DatabaseConnection("127.0.0.1", user="root", passwd='xlsw', db='test')
+    dmop = DmhyDataOperator(db);
+    data = dmop.getby_id("23")
+    print data
