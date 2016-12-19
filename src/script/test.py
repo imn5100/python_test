1c6dbe24b28566d3b306e23043f919fed8deaea5
tuanzi <tuanzi@2dfire.com>
2016-12-19 18:43:51 +0800

    add loop_run

--- src/script/test.py
+++ src/script/test.py
@@ -4,6 +4,8 @@ import hashlib
 import json
 import os
 import subprocess
+import time
+from threading import Timer
 
 import requests
 
@@ -23,6 +25,9 @@ consumer_url = "https://shawblog.me/admin/remote/consumerMsg.do"
 # id 操作的任务数据主键  type 1-4 重新启用任务 开启任务 完成任务 任务失败 默认为开启任务
 callback_url = "https://shawblog.me/admin/remote/callBackMsg.do"  # 需要传递md5密码
 
+check_auth_url = "https://shawblog.me/admin/main.jsp"
+check_str = u"管理界面"
+
 
 def md5lower(str):
     md5c = hashlib.md5()
@@ -36,15 +41,28 @@ def get_aria2_client():
     return Aria2JsonRpc(rpc_url, aria2_path)
 
 
-def auth(user_data):
+def auth(session, user_data):
+    if not session:
+        session = requests.session()
     user_data["password"] = md5lower(user_data["password"])
-    s = requests.Session()
-    r = s.post(remote_login_url, user_data, timeout=5)
+    r = session.post(remote_login_url, user_data)
     if r.text == "200":
-        return s
+        return session
     else:
         print(r.text)
         return None
+
+
+def check_auth(session, user_data):
+    if session:
+        r = session.get(check_auth_url)
+        pos = r.text.find(check_str)
+        if pos > 1:
+            return session
+        else:
+            return auth(session, user_data)
+    else:
+        return auth(session, user_data)
 
 
 def download(msg_data):
@@ -81,7 +99,8 @@ def build_file(resultobj):
 
 
 def test_python():
-    s = auth(user_data)
+    s = requests.session()
+    s = check_auth(user_data)
     msg_data = s.get(consumer_url, params={"topic": "python"})
     try:
         resultobj = json.loads(msg_data.text)
@@ -95,5 +114,23 @@ def test_python():
         s.get(callback_url, params={"id": resultobj["data"]["id"], "type": STATUS_MAP["fail"]})
 
 
+def loop_run(fun, interval, num=None):
+    fun()
+    if num:
+        # 如果限定了次数，执行次数范围内
+        num -= 1
+        if num > 0:
+            Timer(interval, loop_run, (fun, interval, num)).start()
+        else:
+            return
+    else:
+        Timer(interval, loop_run, (fun, interval)).start()
+
+
+def print_time():
+    print(str(time.time()))
+
+
 if __name__ == '__main__':
-    test_python()
+    # 半小时执行一次，一直执行
+    loop_run(print_time, 1, 10)
