d9c3d19a4386756de96c32205b5337c1478ca1fb
imn5100 <imn5100_chu2@163.com>
2017-10-16 12:49:46 +0800

    update

--- src/dmhyCrawler/DataCrawler.py
+++ src/dmhyCrawler/DataCrawler.py
@@ -41,18 +41,18 @@ class DmhyData(object):
 
 
 def buildHeader():
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
-    headers['Accept-Encoding'] = 'gzip, deflate, sdch'
-    headers['Content-Type'] = 'gzip'
-    headers['Accept-Language'] = 'zh-CN,zh;q=0.8,en;q=0.6'
-    headers['Connection'] = 'keep-alive'
-    headers['Content-Type'] = 'gzip'
-    headers['Host'] = 'share.dmhy.org'
-    headers['referer'] = 'http://share.dmhy.org'
-    headers[
-        'Cookie'] = '__cfduid=d1a9b70e02b3116c622b3747893a44ef81472616279; Hm_lvt_e4918ccc327a268ee93dac21d5a7d53c=1472616271,1472692420,1472779166,1472803144; Hm_lpvt_e4918ccc327a268ee93dac21d5a7d53c=1472803144'
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/61.0.3163.100 Safari/537.36',
+               'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+               'accept-encoding': 'gzip, deflate, sdch', 'Content-Type': 'gzip',
+               'accept-language': 'zh-CN,zh;q=0.8,en;q=0.6',
+               'cache-control': 'max-age=0',
+               'cookie': '__cfduid=d98205d21eeda32bc056eab6376f4f0a71489391007; HstCfa3801674=1493787344743; '
+                         'HstCla3801674=1493787344743; HstCmu3801674=1493787344743; HstPn3801674=1; HstPt3801674=1; '
+                         'HstCnv3801674=1; HstCns3801674=1; Hm_lvt_e4918ccc327a268ee93dac21d5a7d53c=1506427818,'
+                         '1506736762,1507606164,1508121182; Hm_lpvt_e4918ccc327a268ee93dac21d5a7d53c=1508122612',
+               'if-modified-since': 'Mon, 16 Oct 2017 02:57:19 GMT'
+               }
     return headers
 
 
@@ -98,7 +98,7 @@ def analysisHtml(text):
     return dmhy_datas
 
 
-if __name__ == '__main__':
+def read_dmhy():
     page = 1
     keyword = urllib.pathname2url("")
     searchUrl = "https://share.dmhy.org/topics/list/page/%u?keyword=%s" % (1, keyword);
@@ -108,3 +108,14 @@ if __name__ == '__main__':
     dmhydatas.reverse()
     for data in dmhydatas:
         print (str(data.time) + ":" + data.title)
+
+
+if __name__ == '__main__':
+    read_dmhy()
+    # from hyper import HTTPConnection
+    #
+    # conn = HTTPConnection('share.dmhy.org')
+    # conn.request('GET', '/get', headers=buildHeader())
+    # resp = conn.get_response()
+    #
+    # print(resp.read())
