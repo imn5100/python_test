4ba4215f50a57f01480ad6906f188eb1f40eadfb
imn5100 <imn5100_chu2@163.com>
2018-05-11 18:54:25 +0800

    fix  bug

--- websocket/WebSocketClient.py
+++ websocket/WebSocketClient.py
@@ -1,9 +1,10 @@
 # -*- coding=utf-8 -*-
 import codecs
 import json
+import urlparse
 
 from tornado import escape, gen, httpclient, httputil, ioloop, websocket
-
+from urllib import urlencode
 from Frame import Frame
 
 APPLICATION_JSON = 'application/json'
@@ -22,6 +23,14 @@ CMD_STOMP = 'STOMP'
 CMD_SEND = 'SEND'
 CMD_SUBSCRIBE = 'SUBSCRIBE'
 CMD_UNSUBSCRIBE = 'UNSUBSCRIBE'
+
+
+def add_url_params(url, params):
+    url_parts = list(urlparse.urlparse(url))
+    query = dict(urlparse.parse_qsl(url_parts[4]))
+    query.update(params)
+    url_parts[4] = urlencode(query)
+    return urlparse.urlunparse(url_parts)
 
 
 class WebSocketClient:
@@ -83,21 +92,31 @@ class WebSocketClient:
         pass
 
 
-class StompWebSocketClient(WebSocketClient):
+class DoloresStompWebSocketClient(WebSocketClient):
+
+    def __init__(self, token, driver_id, user_id):
+        WebSocketClient.__init__(self)
+        self.token = token
+        self.driver_id = driver_id
+        self.user_id = user_id
+        self.sub_topic = '/dolores/driver/%s/%s' % (self.user_id, self.driver_id)
+
+    def connect(self, url):
+        url = add_url_params(url, {'token': self.token})
+        WebSocketClient.connect(self, url)
 
     def _on_message(self, msg):
         if msg.startswith('a["') and msg.endswith("\u0000\"]"):
-            msg = u'\u0000'.join(msg.rsplit('\u0000', 1))
-            msg = codecs.escape_decode(bytes(msg))[0].decode('utf-8')
-            msg = msg[3:-3]
-        print(Frame.read_message_to_frame(msg))
+            msg = u'\u0000'.join(msg.rsplit('\u0000', 1)).encode('utf-8').decode('string-escape')[3:-3]
+        msg = Frame.read_message_to_frame(msg)
+        if msg.cmd == 'MESSAGE':
+            print(msg)
 
     def _on_connection_success(self):
         print('Connected!')
-        sub_topic = '/dolores/driver/1/fe73bd5d5e173c80e6ec7c23694a3af802a66c75008a2cb4'
         conn_frame = Frame(CMD_CONNECT,
                            headers={'accept=version': '1.1,1.0', 'heart-beat': '10000,10000'})
-        sub_frame = Frame(CMD_SUBSCRIBE, headers={'id': 'sub-0', 'destination': sub_topic})
+        sub_frame = Frame(CMD_SUBSCRIBE, headers={'id': 'sub-0', 'destination': self.sub_topic})
         self.send(conn_frame.convert_to_data())
         self.send(sub_frame.convert_to_data())
 
@@ -109,11 +128,18 @@ class StompWebSocketClient(WebSocketClient):
         print('Connection error: %s', exception)
         exit(0)
 
+    def send_frame_msg(self, destination, msg):
+        msg_body = json.dumps({'name': msg})
+        frame = Frame(CMD_SEND, headers={'destination': destination, 'content-length': len(msg_body)},
+                      body=msg_body)
+        print(frame.convert_to_data())
+        self.send(frame.convert_to_data())
+
 
 def main():
-    client = StompWebSocketClient()
-    token = '17e4e9af155210ebeea8ffb2ec70b9cc'
-    client.connect('ws://wst.shawblog.me/dolores/873/qdqavh3w/websocket?token=' + token)
+    token = '4a08398861816f734436c6b10f1d1493'
+    client = DoloresStompWebSocketClient(token, 'fe73bd5d5e173c80e6ec7c23694a3af802a66c75008a2cb4', 1)
+    client.connect('ws://wst.shawblog.me/dolores/873/qdqavh3w/websocket')
     try:
         ioloop.IOLoop.instance().start()
     except KeyboardInterrupt:
